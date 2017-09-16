a6851d6d23984506887a5c32378c8bf7db8a21f5
lingbao <852347376@qq.com>
2017-09-16 11:04:31 +0800

    添加tabbar

---
+++ GoddessHairSalonSwiftDemo/GoddessHairSalonSwiftDemo/SecondVC.swift
@@ -0,0 +1,24 @@
+//
+//  SecondVC.swift
+//  GoddessHairSalonSwiftDemo
+//
+//  Created by galaxy on 2017/9/16.
+//  Copyright © 2017年 galaxy. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+}
---
+++ GoddessHairSalonSwiftDemo/GoddessHairSalonSwiftDemo/ThindVC.swift
@@ -0,0 +1,24 @@
+//
+//  ThindVC.swift
+//  GoddessHairSalonSwiftDemo
+//
+//  Created by galaxy on 2017/9/16.
+//  Copyright © 2017年 galaxy. All rights reserved.
+//
+
+import UIKit
+
+class ThindVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+}
